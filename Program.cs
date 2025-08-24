705217c9ad5742e622424b84e7a4553618f50cf7
trongpq091999@gmail.com <trongpq091999@gmail.com>
2025-08-24 23:46:10 +0700

    first setup dapr

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Dapr.Client;
+
+class Program
+{
+    const string storeName = "statestore";
+    const string key = "counter";
+
+    static async Task Main(string[] args)
+    {
+        var daprClient = new DaprClientBuilder().Build();
+        var counter = await daprClient.GetStateAsync<int>(storeName, key);
+
+        while (true)
+        {
+            Console.WriteLine($"Counter = {counter++}");
+            await daprClient.SaveStateAsync(storeName, key, counter);
+            await Task.Delay(1000);
+        }
+    }
+}
